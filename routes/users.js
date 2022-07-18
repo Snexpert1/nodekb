2536efaeb75bbdac1357154f6c61aeb10fe3e9f9
Snexpert1 <92059953+Snexpert1@users.noreply.github.com>
2022-07-18 16:48:14 +1000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,70 +1,104 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const passport = require('passport');
-const { User } = require('../models/user');
 
-// Register Form
-router.get('/register', async (req, res) => {
-  res.render('register');
+//bring the users model
+let User = require('../models/users');
+
+// Register form router
+
+router.get('/register', (req,res)=>{
+    res.render('register');
+})
+
+router.post('/register', (req,res)=>{
+    const name = req.body.name;
+    const username= req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+    
+
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not correct').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords not match').equals(req.body.password);
+
+    let errors = req.validationErrors();
+
+    if (errors){
+        res.render('register', {
+            errors:errors
+        });
+
+
+    }else{
+        let newUser = new User({
+            name:name,
+            email:email,
+            username:username,
+            password:password
+
+        });
+        bcrypt.genSalt(10, (err,salt)=>{
+            bcrypt.hash(newUser.password, salt, (err,hash)=>{
+                if (err){
+                    console.log(err);
+                }
+                newUser.password = hash;
+                newUser.save((err)=>{
+                        if (err){
+                            console.log(err);
+                            return;
+                        }else{
+                            req.flash('success', 'You are now registered and can log in');
+                            res.redirect('/users/login');
+                        }
+            
+                    });
+                
+            });
+        
+
+        });
+
+    }
+
+
+
+});
+// Login form route
+router.get('/login', (req,res)=>{
+    res.render('login');
 });
 
-// Register Proccess
-router.post('/register', async (req, res) => {
+//Login post process
 
-  const name = req.body.name;
-  const email = req.body.email;
-  const username = req.body.username;
-  const password = req.body.password;
-  const password2 = req.body.password2;
+router.post('/login', (req,res, next)=>{
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req,res,next);
 
-  req.checkBody('name', 'Name is required').notEmpty();
-  req.checkBody('email', 'Email is required').notEmpty();
-  req.checkBody('email', 'Email is not valid').isEmail();
-  req.checkBody('username', 'Username is required').notEmpty();
-  req.checkBody('password', 'Password is required').notEmpty();
-  req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+});
 
-  let errors = req.validationErrors();
+//route for logout
 
-  if (errors) {
-    res.render('register', {
-      errors: errors
+router.get('/logout', function(req,res){
+    req.logout((err)=>{
+        if (err) {
+            throw err;
+        }else {
+            res.redirect('/users/login');
+        };
     });
-  } else {
-    const salt = await bcrypt.genSalt(10);
-    const newUser = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      username: req.body.username,
-      password: await bcrypt.hash(req.body.password, salt)
-    });
-    newUser.save();
-    req.flash('success', 'You are now registered and can log in');
-    res.redirect('/users/login');
-  }
-});
+    req.flash('success', 'You are logged out!');
+    
 
-
-// Login Form
-router.get('/login', async (req, res) => {
-  res.render('login');
-});
-
-// Login Process
-router.post('/login', async (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/users/login',
-    failureFlash: true
-  })(req, res, next);
-});
-
-// logout
-router.get('/logout', async (req, res) => {
-  req.logout();
-  req.flash('success', 'You are logged out');
-  res.redirect('/users/login');
 });
 
 module.exports = router;
