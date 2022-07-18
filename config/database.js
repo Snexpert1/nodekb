cd6237c6c4225a28b71239980f70b2566ec39e06
Snexpert1 <92059953+Snexpert1@users.noreply.github.com>
2022-07-18 16:39:42 +1000

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,4 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  });
-
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
-};
-
-module.exports = connectDB; 
+module.exports = {
+    database: 'mongodb://localhost:27017/nodekb',
+    secret: 'silentsecret'
+}
